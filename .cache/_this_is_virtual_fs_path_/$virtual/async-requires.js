530be8010b639ce1d5fba7dcda9b42a7ca7f568f
Gustavo Freitas <gusta.justino.freitas@gmail.com>
2022-06-21 14:52:22 -0300

    update

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/async-requires.js
@@ -0,0 +1,5 @@
+exports.components = {
+  "component---node-modules-lekoarts-gatsby-theme-cara-src-templates-cara-tsx": () => import("./../../../node_modules/@lekoarts/gatsby-theme-cara/src/templates/cara.tsx" /* webpackChunkName: "component---node-modules-lekoarts-gatsby-theme-cara-src-templates-cara-tsx" */),
+  "component---src-pages-404-jsx": () => import("./../../../src/pages/404.jsx" /* webpackChunkName: "component---src-pages-404-jsx" */)
+}
+
---
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,10 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---node-modules-lekoarts-gatsby-theme-cara-src-templates-cara-tsx": preferDefault(require("/home/justino/Documentos/Desenvolvimento/JavaScript/Node/portfolio/node_modules/@lekoarts/gatsby-theme-cara/src/templates/cara.tsx")),
+  "component---src-pages-404-jsx": preferDefault(require("/home/justino/Documentos/Desenvolvimento/JavaScript/Node/portfolio/src/pages/404.jsx"))
+}
+
